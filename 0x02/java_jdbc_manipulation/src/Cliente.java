9af1ff6e9db5e19a985ef16861607cd1784a488d
alepereiraciandt <alexandrepereira@ciandt.com>
2022-06-27 17:42:16 -0300

    fix

--- 0x02/java_jdbc_manipulation/src/Cliente.java
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Cliente {
+public class Cliente {
+    private int id;
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String RG;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getRG() {
+        return RG;
+    }
 }
