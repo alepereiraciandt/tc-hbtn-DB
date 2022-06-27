71d07743291840dbab6aa2eac9fc025631ac5866
alepereiraciandt <alexandrepereira@ciandt.com>
2022-06-27 11:35:09 -0300

    fix

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class PrintJDBCDrivers {
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class PrintJDBCDrivers {
+    public static void main(String[] args) {
+        System.out.println("List of loaded JDBC drivers");
+
+        for(Enumeration<Driver> e = DriverManager.getDrivers(); e.hasMoreElements();) {
+            Driver driver = e.nextElement();
+            print(driver);
+        }
+    }
+
+    public static void print(Driver driver) {
+        String className = driver.getClass().getName();
+        int majorVersion = driver.getMajorVersion();
+        int miniorVersion = driver.getMinorVersion();
+
+
+        System.out.println("------------------------");
+        System.out.println("Name driver: " + className);
+        System.out.println("Driver major version " + majorVersion);
+        System.out.println("Driver minor version " + miniorVersion);
+        System.out.println("------------------------");
+    }
 }
